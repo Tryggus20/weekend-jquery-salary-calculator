2671392440d5f70a1b0ea257bec76505db6e9f54
David Campbell <tryggus20@gmail.com>
2023-09-02 13:04:45 -0500

    monthly salary counts up correctly and delete button removes but does not update #

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(readyNow);
 function readyNow() {
 console.log("hello js and jq"); 
 $(".submit").on("click", addInput)
+$(".table").on("click", ".deleteButton", deleteRow)
 
 }// end of readyNow
 
@@ -13,7 +14,8 @@ $(".submit").on("click", addInput)
    delete button needs to change the calculator to correcly reflect 
 */
 let annualSalary = 0 // outside of function so my delete button can access it
-
+let totalSalary =0
+let totalMonth = 0
 function addInput() {
     console.log('button says "hello"'); //button working
     let firstName = $(".firstName").val();
@@ -22,8 +24,14 @@ function addInput() {
     let title = $('.title').val();
     annualSalary = Number ($('.annualSalary').val());
     console.log(annualSalary);
+
+    // if (!firstName || !lastName || !id || !title || annualSalary <= 0) {
+    //     alert("Please fill in all fields with the correct information");
+    //     return; //alert for missing data comment on/off for testing
+    
+
     $(`.table`).append(
-    `<tr>
+    `<tr class ="removable">
      <td>${firstName}</td>
      <td>${lastName}</td>
      <td>${id}</td>
@@ -32,5 +40,14 @@ function addInput() {
      <td class="delete"><button class="deleteButton">Delete</button></td>
      </tr>
 `) //end of append
-
+totalSalary = annualSalary + totalSalary
+totalMonth = (totalSalary/12)
+$(".monthlySalary").text(totalMonth);
+console.log(`Total Salary: ${totalSalary} Monthly Salary: ${totalMonth}`); // added for testing
 } //end of addInput function
+
+function deleteRow (event){
+    console.log("deletus!");
+annualSalary = 
+    $(event.target).closest("tr").remove();
+}
